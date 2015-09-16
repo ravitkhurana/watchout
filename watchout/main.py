96c3a1dc1f7dca7db806d891139c26139370147d
Ravit Khurana <ravit.khurana@gmail.com>
2015-09-16 16:28:11 +0530

    Done some refactoring
    1. Created new class Obstacles to contain the obstacles.
    2. Moved code to update obstacles from GameState to Obstacles.
    3. Added update() method to the class Player.

--- watchout/main.py
+++ watchout/main.py
@@ -3,6 +3,7 @@ import enum
 
 
 class Color(enum.Enum):
+
     """
     Enumeration for common colors
     """
@@ -14,22 +15,24 @@ class Color(enum.Enum):
 
 
 class Constants(object):
+
     """
     Class to hold constants used in game
     """
     GRAVITY = 1
+    RESOLUTION = (640, 480)
+    ROAD_Y = 400
 
 
 class GameState(object):
+
     """
     Class to maintain current state of the game
     """
+
     def __init__(self):
         self.player = Player()
-        self.obstacles = list()
-        self.prev_obstacle = None
-        self.resolution = None
-        self.road_y = None
+        self.obstacles = Obstacles()
         self.is_game_over = False
         self.score = 0
 
@@ -39,32 +42,10 @@ class GameState(object):
         for collisions and incrementing the score. Set self.is_game_over if
         a collision is detected.
         """
-        self.update_obstacles()
+        self.player.update()
+        self.obstacles.update()
         self.check_collision()
         self.score += 1
-
-    def spawn_obstacle(self):
-        """
-        This method spawns new obstacles if required.
-        """
-        if self.prev_obstacle is None or self.resolution[0] - self.prev_obstacle.position[0] > 200:
-            obstacle_dim = (20, 30)
-            obstacle_pos = [self.resolution[0], self.road_y - obstacle_dim[1]]
-            self.prev_obstacle = Obstacle(obstacle_dim, obstacle_pos)
-            self.obstacles.append(self.prev_obstacle)
-
-    def update_obstacles(self):
-        """
-        This method updates the position of all obstacles
-        """
-        self.spawn_obstacle()
-        obstacles_to_be_removed = list()
-        for obstacle in self.obstacles:
-            obstacle.position[0] -= 5
-            if obstacle.position[0] + obstacle.dimensions[0] < 0:
-                obstacles_to_be_removed.append(obstacle)
-        for obstacle in obstacles_to_be_removed:
-            self.obstacles.remove(obstacle)
 
     def check_collision(self):
         """
@@ -95,107 +76,163 @@ class GameState(object):
 
 
 class Player(object):
+
     """
     Class to maintain player's state.
     """
+
     def __init__(self):
         self.dimensions = None
         self.position = None
         self.in_jump = False
-        self.position_before_jump = None
-        self.speed = 0
+        self.velocity = [0, 0]
 
     def jump(self):
         """
         This method initiates a jump if player is not already in between a
         jump. Otherwise, it just updates player's position according to
-        player's current speed and value of Constants.GRAVITY.
+        player's current velocity and value of Constants.GRAVITY.
         """
         if not self.in_jump:
-            self.speed = 15
-            self.position_before_jump = (self.position[0], self.position[1])
+            self.velocity = [0, 15]
             self.in_jump = True
-        t = 1
-        distance = (self.speed * t) - (Constants.GRAVITY * (t ^ 2))
-        self.speed -= Constants.GRAVITY * t
-        self.position[1] -= distance
-        if self.position[1] > self.position_before_jump[1]:
-            self.position[1] = self.position_before_jump[1]
-            self.in_jump = False
-            self.position_before_jump = None
+
+    def update(self):
+        """
+        This method updates player's position if the player is already in
+        motion.
+        """
+        if self.in_jump:
+            t = 1
+            distance = (self.velocity[1] * t) - (Constants.GRAVITY * (t ^ 2))
+            self.velocity[1] -= Constants.GRAVITY * t
+            self.position[1] -= distance
+            if self.position[1] > Constants.ROAD_Y - self.dimensions[1]:
+                self.position[1] = Constants.ROAD_Y - self.dimensions[1]
+                self.in_jump = False
+                self.velocity[1] = 0
 
 
 class Obstacle(object):
+
     """
     Class to maintain an obstacle's state.
     """
-    def __init__(self):
-        self.dimensions = None
-        self.position = None
 
     def __init__(self, dim, pos):
         self.dimensions = dim
         self.position = pos
 
 
-def main():
+class Obstacles(list):
+
     """
-    Main method which starts the game.
+    Class to contain and manage all the obstacles
     """
-    pygame.init()
-    # Initialize game state
-    game_state = GameState()
-    game_state.resolution = (640, 480)
-    game_state.road_y = 400
-    game_state.player.dimensions = (20, 50)
-    game_state.player.position = [50, game_state.road_y - game_state.player.dimensions[1]]
-    screen = pygame.display.set_mode(game_state.resolution)
-    pygame.display.set_caption("Watch Out!")
-    clock = pygame.time.Clock()
-    done = False
-    # Game loop
-    while not done:
-        # * Process events in the events queue
-        player_jump = False
-        for event in pygame.event.get():
-            assert isinstance(event, pygame.event.EventType)
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    player_jump = True
-                if event.key == pygame.K_ESCAPE:
+
+    def __init__(self):
+        super().__init__()
+
+    def spawn_obstacle(self):
+        """
+        This method spawns new obstacles if required.
+        """
+        if len(self) == 0 or Constants.RESOLUTION[0] - self[-1].position[0] > 200:
+            obstacle_dim = (20, 30)
+            obstacle_pos = [Constants.RESOLUTION[0], Constants.ROAD_Y - obstacle_dim[1]]
+            self.append(Obstacle(obstacle_dim, obstacle_pos))
+
+    def update(self):
+        """
+        This method updates the position of all obstacles
+        """
+        self.spawn_obstacle()
+        obstacles_to_be_removed = list()
+        for obstacle in self:
+            obstacle.position[0] -= 5
+            if obstacle.position[0] + obstacle.dimensions[0] < 0:
+                obstacles_to_be_removed.append(obstacle)
+        for obstacle in obstacles_to_be_removed:
+            self.remove(obstacle)
+
+
+class Game(object):
+
+    """
+    The game class
+    """
+
+    def __init__(self):
+        self.restart = True
+
+    def start(self):
+        """
+        This method starts the game
+        """
+        while self.restart:
+            self.restart = False
+            self.main()
+
+    def main(self):
+        """
+        Main method which runs the game.
+        """
+        pygame.init()
+        # Initialize game state
+        game_state = GameState()
+        game_state.player.dimensions = (20, 50)
+        game_state.player.position = [50, Constants.ROAD_Y - game_state.player.dimensions[1]]
+        screen = pygame.display.set_mode(Constants.RESOLUTION)
+        pygame.display.set_caption("Watch Out!")
+        clock = pygame.time.Clock()
+        done = False
+        # Game loop
+        while not done:
+            # * Process events in the events queue
+            player_jump = False
+            for event in pygame.event.get():
+                assert isinstance(event, pygame.event.EventType)
+                if event.type == pygame.QUIT:
                     done = True
-        if not game_state.is_game_over:
-            # * Perform calculations for movements, collision detection, etc.
-            if player_jump or game_state.player.in_jump:
-                game_state.player.jump()
-            game_state.update()
-        # * Draw on screen
-        screen.fill(Color.WHITE.value)
-        font = pygame.font.SysFont('Calibri', 25, True, False)
-        if not game_state.is_game_over:
-            text = font.render("Watch out for the bluh-dy rocks!", True, Color.BLACK.value)
-        else:
-            text = font.render("Game Over Mayte!", True, Color.RED.value)
-        screen.blit(text, ((screen.get_width() - text.get_width())/2, 50))
-        score_text = font.render("Your score: " + game_state.score.__str__(), True, Color.BLUE.value)
-        screen.blit(score_text, ((screen.get_width() - score_text.get_width())/2, score_text.get_height() + 60))
-        # Draw player
-        player_x, player_y = game_state.player.position
-        player_w, player_h = game_state.player.dimensions
-        pygame.draw.rect(screen, Color.BLUE.value, [player_x, player_y, player_w, player_h], 0)
-        # Draw obstacles
-        for obstacle in game_state.obstacles:
-            obstacle_x, obstacle_y = obstacle.position
-            obstacle_w, obstacle_h = obstacle.dimensions
-            pygame.draw.rect(screen, Color.RED.value, [obstacle_x, obstacle_y, obstacle_w, obstacle_h], 0)
-        # * Refresh screen
-        pygame.display.flip()
-        # * Set maximun FPS
-        clock.tick(60)
-    pygame.quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player_jump = True
+                    if event.key == pygame.K_r:
+                        self.restart = True
+                        done = True
+                    if event.key == pygame.K_ESCAPE:
+                        done = True
+            if not game_state.is_game_over:
+                # * Perform calculations for movements, collision detection, etc.
+                if player_jump:
+                    game_state.player.jump()
+                game_state.update()
+            # * Draw on screen
+            screen.fill(Color.WHITE.value)
+            font = pygame.font.SysFont('Calibri', 25, True, False)
+            if not game_state.is_game_over:
+                text = font.render("Watch out for the bluh-dy rocks!", True, Color.BLACK.value)
+            else:
+                text = font.render("Game Over Mayte!", True, Color.RED.value)
+            screen.blit(text, ((screen.get_width() - text.get_width())/2, 50))
+            score_text = font.render("Your score: " + game_state.score.__str__(), True, Color.BLUE.value)
+            screen.blit(score_text, ((screen.get_width() - score_text.get_width())/2, score_text.get_height() + 60))
+            # Draw player
+            player_x, player_y = game_state.player.position
+            player_w, player_h = game_state.player.dimensions
+            pygame.draw.rect(screen, Color.BLUE.value, [player_x, player_y, player_w, player_h], 0)
+            # Draw obstacles
+            for obstacle in game_state.obstacles:
+                obstacle_x, obstacle_y = obstacle.position
+                obstacle_w, obstacle_h = obstacle.dimensions
+                pygame.draw.rect(screen, Color.RED.value, [obstacle_x, obstacle_y, obstacle_w, obstacle_h], 0)
+            # * Refresh screen
+            pygame.display.flip()
+            # * Set maximun FPS
+            clock.tick(60)
+        pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.start()
